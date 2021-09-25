a788f15e740b28913f31d7b3680bc544dce61be5
Isaiah186 <91351729+Isaiah186@users.noreply.github.com>
2021-09-25 17:31:49 -0500

    Add files via upload

---
+++ For Loop Multiples Homework (1).py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]
+
+
+# In[ ]:
+
+
+
+
+
+# In[3]:
+
+
+for x in numbers:
+    if x%6==0:
+        print(x)
+
+
+# In[4]:
+
+
+for x in numbers:
+    if x%3==0:
+        print(x)
+        
+
+
+# In[5]:
+
+
+for x in numbers:
+    if x%10==0:
+        print(x)
+
+
+# In[7]:
+
+
+for x in numbers:
+    if x%2==0:
+        print(x)
+
+
+# In[8]:
+
+
+for x in numbers:
+    if x%2!=0:
+        print(x)
+
+
+# In[ ]:
+
+
+#For loops are for repeating a certain sequence
+#We use for loops to cycle through for everything for us and see if what we are doing meets the requirements to be ran in the loop
+
